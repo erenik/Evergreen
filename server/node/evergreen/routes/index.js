7a5659d709f40a4038c5369d9de0739f16a4ccba
Emil Hedemalm <emil_hedemalm@hotmail.com>
2016-11-16 22:52:27 +0100

    Web page update

--- server/node/evergreen/routes/index.js
+++ server/node/evergreen/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Evergreen' });
+  res.render('index', { title: 'Erenik' });
+});
+
+router.get('/evergreen', function(req, res, next) {
+  res.render('evergreen/index', { title: 'Evergreen' });
 });
 
 module.exports = router;
